72e38a05e0096f34126f0738da22a2577666ff5b
chdattat <chillal.dattatreya@capgemini.com>
2020-10-12 15:38:07 +0530

    Updated Test cases failure fix

--- module/src/test/java/com/example/TestGreeter.java
+++ module/src/test/java/com/example/TestGreeter.java
@@ -31,17 +31,11 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
   
-  @Test
-  public void greetShouldIncludeGreetingMessage() {
-    String someone = "World, Thanks you very much";
-
-    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-  }
 }
